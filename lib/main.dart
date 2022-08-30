8940e609579813a2577004556fd408a09061f2fb
ilhuitem0c <jc11111001001@gmail.com>
2022-08-30 15:29:14 -0500

    first Column and Cards

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:two_expenses_app/transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1', 
+      title: 'New Shoes', 
+      amount: 69.99, 
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2', 
+      title: 'Despensa', 
+      amount: 16.53, 
+      date: DateTime.now(),
+    ),
+  ]; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gastos'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container( 
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Grafica'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            color: Colors.red,
+            child: Text('Lista de gastos'),
+          ),
+        ],
+      ),
+    );
+  }
+}
