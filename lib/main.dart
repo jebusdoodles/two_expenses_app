440be23b25f5d2e31b0997ece2af71da486cec35
ilhuitem0c <jc11111001001@gmail.com>
2022-08-31 15:32:41 -0500

    Some styling

--- lib/main.dart
+++ lib/main.dart
@@ -48,9 +48,55 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('Lista de gastos'),
+          Column(
+            children: transactions.map(
+              (tx){
+                return Card(
+                  child: Row(
+                    children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10, 
+                            horizontal: 15),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple, 
+                              width: 2, 
+                            )
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$ ${tx.amount}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple, 
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontSize: 16, 
+                                fontWeight: FontWeight.bold
+                              )
+                            ),
+                            Text(
+                              tx.date.toString(),
+                              style: TextStyle(
+                                color: Colors.grey, 
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ), 
+                ); 
+              }
+            ).toList(),
           ),
         ],
       ),
