fda20c298e65060c9013b253f83d663a0196cb3d
ilhuitem0c <jc11111001001@gmail.com>
2022-09-08 14:36:42 -0500

    Some stylising

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber, 
+      ),
       home: MyHomePage(),
     );
   }
@@ -70,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gastos'),
+        title: Text('Personal Expenses'),
         actions: [
           IconButton(icon: Icon(
             Icons.add
